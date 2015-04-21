d3eea42c7e8127ea40e544eec6a1d969e8efec8b
Varun Rajgopal <rajgopav@onid.oregonstate.edu>
2015-04-20 21:33:42 -0700

    Update mergesort.c

--- projects/rajgopav/Assign1/mergesort.c
+++ projects/rajgopav/Assign1/mergesort.c
@@ -1,19 +1,23 @@
 #include "mergesort.h"
 #include <stdio.h>
 
+//Function to merge the two haves arr[l..m] and arr[m+1..r] of array arr[]
 void merge(int arr[], int l, int m, int r)
 {
     int i, j, k;
     int n1 = m - l + 1;
     int n2 =  r - m;
 
+    /* create temp arrays */
     int L[n1], R[n2];
 
+    /* Copy data to temp arrays L[] and R[] */
     for(i = 0; i < n1; i++)
         L[i] = arr[l + i];
     for(j = 0; j < n2; j++)
         R[j] = arr[m + 1+ j];
 
+    /* Merge the temp arrays back into arr[l..r]*/
     i = 0;
     j = 0;
     k = l;
@@ -31,14 +35,16 @@ void merge(int arr[], int l, int m, int r)
         }
         k++;
     }
-
+    
+    /* Copy the remaining elements of L[], if there are any */
     while (i < n1)
     {
         arr[k] = L[i];
         i++;
         k++;
     }
-
+    
+    /* Copy the remaining elements of R[], if there are any */
     while (j < n2)
     {
         arr[k] = R[j];
@@ -47,18 +53,21 @@ void merge(int arr[], int l, int m, int r)
     }
 }
 
+/*l is for left index and r is right index of the sub-array
+  of arr to be sorted */
 void mergeSort(int arr[], int l, int r)
 {
     if (l < r)
     {
-        int m = l+(r-l)/2;
+        int m = l+(r-l)/2; //Same as (l+r)/2, but avoids overflow for large l and h
         mergeSort(arr, l, m);
         mergeSort(arr, m+1, r);
         merge(arr, l, m, r);
     }
-
-        for(int i=0; i<r; i++)
-        {
-                printf("Elements of the array[%d]: %d ",i,arr[i]);
-        }
+    
+    //Print all the elements in the array
+    for(int i=0; i<r; i++)
+    {
+            printf("Elements of the array[%d]: %d ",i,arr[i]);
+    }
 }
