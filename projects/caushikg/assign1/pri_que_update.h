63a84bb48f985a6a6823050b20e5f150e7926605
gcaushik <caushik1@gmail.com>
2015-04-21 11:18:54 -0700

    Added header files

---
+++ projects/caushikg/assign1/pri_que_update.h
@@ -0,0 +1,14 @@
+#ifndef PRIORITY_QUEUE_H
+#define PRIORITY_QUEUE_H
+
+void create();
+void insert_by_priority(int data);
+void check(int data);
+void delete_by_priority();
+void display_pqueue();
+
+unsigned int pri_que[MAX];
+
+int front, rear;
+
+#endif
